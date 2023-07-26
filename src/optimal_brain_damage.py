4fa5f9c2658e610a58ca404e6799fdc73c5cf11f
Ali Rad <hiairno@gmail.com>
2023-07-26 14:50:56 -0400

    Create optimal_brain_damage.py

---
+++ src/optimal_brain_damage.py
@@ -0,0 +1,6 @@
+"""
+This script uses TensorFlow and Numba to simulate a Quantum Many-Body system,
+applying tensor computations to perform the simulation. 
+The script covers various operations including creating a Hamiltonian,
+optimizing and pruning the system, and calculating the cost function.
+"""
